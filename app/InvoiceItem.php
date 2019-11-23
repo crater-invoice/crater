06807275b99f78a95e82a32a442a351953ebc3b6
Mohit Panjwani <mohit.panjvani@gmail.com>
2019-11-23 11:54:35 +0530

    fix item reports issue

--- app/InvoiceItem.php
+++ app/InvoiceItem.php
@@ -78,7 +78,8 @@ class InvoiceItem extends Model
     public function scopeItemAttributes($query)
     {
         $query->select(
-            DB::raw('sum(quantity) as total_quantity, sum(total) as total_amount, item_id')
-        )->groupBy('item_id');
+            DB::raw('sum(quantity) as total_quantity, sum(total) as total_amount, invoice_items.name')
+        )->groupBy('invoice_items.name');
+
     }
 }

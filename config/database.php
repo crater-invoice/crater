f0107129fbea072488ef78f40456066c2f692104
raishvaria <raishvaria1996@gmail.com>
2019-11-28 16:06:18 +0530

    fix groupby issue on mysql

--- config/database.php
+++ config/database.php
@@ -53,6 +53,15 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'modes' => [
+                //'ONLY_FULL_GROUP_BY', // Disable this to allow grouping by one column
+                'STRICT_TRANS_TABLES',
+                'NO_ZERO_IN_DATE',
+                'NO_ZERO_DATE',
+                'ERROR_FOR_DIVISION_BY_ZERO',
+                'NO_AUTO_CREATE_USER',
+                'NO_ENGINE_SUBSTITUTION'
+            ],
         ],
 
         'pgsql' => [

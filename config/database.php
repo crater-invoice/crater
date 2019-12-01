80014b02b55140f274db16da09430b57ec8a9a0c
Mohit Panjwani <mohit.panjvani@gmail.com>
2019-12-01 11:22:28 +0530

    fix sql-mode issue

--- config/database.php
+++ config/database.php
@@ -51,17 +51,8 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
-            'modes' => [
-                //'ONLY_FULL_GROUP_BY', // Disable this to allow grouping by one column
-                'STRICT_TRANS_TABLES',
-                'NO_ZERO_IN_DATE',
-                'NO_ZERO_DATE',
-                'ERROR_FOR_DIVISION_BY_ZERO',
-                'NO_AUTO_CREATE_USER',
-                'NO_ENGINE_SUBSTITUTION'
-            ],
         ],
 
         'pgsql' => [

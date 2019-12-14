7df06fb005b43876d4edc25cf8894aa512123953
Mohit Panjwani <mohit.panjvani@gmail.com>
2019-12-14 12:35:32 +0530

    fix invoice number validation

--- app/Listeners/Updates/v2/Version200.php
+++ app/Listeners/Updates/v2/Version200.php
@@ -6,7 +6,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Database\Schema\Blueprint;
 use Crater\Listeners\Updates\Listener;
-use Crater\Listeners\Updates\v2\Version200;
 use Crater\Events\UpdateFinished;
 use Crater\Setting;
 use Crater\Address;

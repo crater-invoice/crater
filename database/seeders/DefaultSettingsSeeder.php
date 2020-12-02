b3e3839fb5c02367517f04bccd4cb0ef1e4dc386
Mohit Panjwani <mohit.panjvani@gmail.com>
2020-12-02 18:52:25 +0530

    fix default seeder

--- database/seeders/DefaultSettingsSeeder.php
+++ database/seeders/DefaultSettingsSeeder.php
@@ -18,7 +18,30 @@ class DefaultSettingsSeeder extends Seeder
     {
         $user = User::where('role', 'super admin')->first();
 
+        $defaultInvoiceEmailBody = 'You have received a new invoice from <b>{COMPANY_NAME}</b>.</br>Please download using the button below:';
+        $defaultEstimateEmailBody = 'You have received a new estimate from <b>{COMPANY_NAME}</b>.</br>Please download using the button below:';
+        $defaultPaymentEmailBody = 'Thank you for the payment.</b></br>Please download your payment receipt using the button below:';
+        $billingAddressFormat = '<h3>{BILLING_ADDRESS_NAME}</h3><p>{BILLING_ADDRESS_STREET_1}</p><p>{BILLING_ADDRESS_STREET_2}</p><p>{BILLING_CITY}  {BILLING_STATE}</p><p>{BILLING_COUNTRY}  {BILLING_ZIP_CODE}</p><p>{BILLING_PHONE}</p>';
+        $shippingAddressFormat = '<h3>{SHIPPING_ADDRESS_NAME}</h3><p>{SHIPPING_ADDRESS_STREET_1}</p><p>{SHIPPING_ADDRESS_STREET_2}</p><p>{SHIPPING_CITY}  {SHIPPING_STATE}</p><p>{SHIPPING_COUNTRY}  {SHIPPING_ZIP_CODE}</p><p>{SHIPPING_PHONE}</p>';
+        $companyAddressFormat = '<h3><strong>{COMPANY_NAME}</strong></h3><p>{COMPANY_ADDRESS_STREET_1}</p><p>{COMPANY_ADDRESS_STREET_2}</p><p>{COMPANY_CITY} {COMPANY_STATE}</p><p>{COMPANY_COUNTRY}  {COMPANY_ZIP_CODE}</p><p>{COMPANY_PHONE}</p>';
+        $paymentFromCustomerAddress = '<h3>{BILLING_ADDRESS_NAME}</h3><p>{BILLING_ADDRESS_STREET_1}</p><p>{BILLING_ADDRESS_STREET_2}</p><p>{BILLING_CITY} {BILLING_STATE} {BILLING_ZIP_CODE}</p><p>{BILLING_COUNTRY}</p><p>{BILLING_PHONE}</p>';
+
         $settings = [
+            'invoice_auto_generate' => 'YES',
+            'payment_auto_generate' => 'YES',
+            'estimate_auto_generate' => 'YES',
+            'save_pdf_to_disk' => 'NO',
+            'invoice_mail_body' => $defaultInvoiceEmailBody,
+            'estimate_mail_body' => $defaultEstimateEmailBody,
+            'payment_mail_body' => $defaultPaymentEmailBody,
+            'invoice_company_address_format' => $companyAddressFormat,
+            'invoice_shipping_address_format' => $shippingAddressFormat,
+            'invoice_billing_address_format' => $billingAddressFormat,
+            'estimate_company_address_format' => $companyAddressFormat,
+            'estimate_shipping_address_format' => $shippingAddressFormat,
+            'estimate_billing_address_format' => $billingAddressFormat,
+            'payment_company_address_format' => $companyAddressFormat,
+            'payment_from_customer_address_format' => $paymentFromCustomerAddress,
             'currency' => 1,
             'time_zone' => 'Asia/Kolkata',
             'language' => 'en',

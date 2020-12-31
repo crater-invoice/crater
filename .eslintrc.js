1be462ef820fa48942c4a4401907ffc0a8c6f620
Mohit Panjwani <mohit.panjvani@gmail.com>
2020-12-31 11:52:10 +0530

    fix eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+}

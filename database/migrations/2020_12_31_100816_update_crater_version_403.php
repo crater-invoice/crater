d4550e62f883e1144d2101578710cc81d272b84e
Mohit Panjwani <mohit.panjvani@gmail.com>
2020-12-31 15:39:01 +0530

    add 403 migration

---
+++ database/migrations/2020_12_31_100816_update_crater_version_403.php
@@ -0,0 +1,29 @@
+<?php
+
+use Crater\Models\Setting;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateCraterVersion403 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Setting::setSetting('version', '4.0.3');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

baebfedf37d2778c487778840257bfd372634cc2
brijraj_04 <bhattbrijraj568@gmail.com>
2021-02-03 12:21:03 +0530

    invoice and estimate from mail name issue

--- app/Mail/SendInvoiceMail.php
+++ app/Mail/SendInvoiceMail.php
@@ -1,6 +1,7 @@
 <?php
 namespace Crater\Mail;
 
+use Config;
 use Crater\Models\EmailLog;
 use Crater\Models\Invoice;
 use Illuminate\Bus\Queueable;
@@ -40,7 +41,7 @@ class SendInvoiceMail extends Mailable
             'mailable_id' => $this->data['invoice']['id']
         ]);
 
-        return $this->from($this->data['from'], env('MAIL_FROM_NAME'))
+        return $this->from($this->data['from'], config('mail.from.name'))
                     ->subject($this->data['subject'])
                     ->markdown('emails.send.invoice', ['data', $this->data]);
     }

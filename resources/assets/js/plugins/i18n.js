17cf0519f68373afefa8ea4d4018b1856223f0ad
Dung <dung.phan@mediabridge.vn>
2021-03-30 08:52:13 +0700

    Initial Japanse and Korean Language

--- resources/assets/js/plugins/i18n.js
+++ resources/assets/js/plugins/i18n.js
@@ -5,10 +5,12 @@ import fr from './fr.json'
 import es from './es.json'
 import ar from './ar.json'
 import de from './de.json'
+import ja from './ja.json'
 import pt_BR from './pt-br.json'
 import it from './it.json'
 import sr from './sr.json'
 import nl from './nl.json'
+import ko from './ko.json'
 import lv from './lv.json'
 import sv from './sv.json'
 import vi from './vi.json'
@@ -23,10 +25,12 @@ const i18n = new VueI18n({
     es,
     ar,
     de,
+    ja,
     pt_BR,
     it,
     sr,
     nl,
+    ko,
     lv,
     sv,
     vi,

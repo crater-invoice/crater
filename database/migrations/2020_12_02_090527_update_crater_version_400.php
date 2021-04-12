e586adde268a6279170b0b5e61cea9f181f4d363
Mohit Panjwani <mohit.panjvani@gmail.com>
2021-04-12 16:43:07 +0530

    update default email description

--- database/migrations/2020_12_02_090527_update_crater_version_400.php
+++ database/migrations/2020_12_02_090527_update_crater_version_400.php
@@ -80,16 +80,16 @@ class UpdateCraterVersion400 extends Migration
 
         FileDisk::create([
             'credentials' => json_encode($publicDisk),
-            'name'=> 'local_public',
-            'type'=> 'SYSTEM',
+            'name' => 'local_public',
+            'type' => 'SYSTEM',
             'driver' => 'local',
             'set_as_default' => false,
         ]);
 
         FileDisk::create([
             'credentials' => json_encode($privateDisk),
-            'name'=> 'local_private',
-            'type'=> 'SYSTEM',
+            'name' => 'local_private',
+            'type' => 'SYSTEM',
             'driver' => 'local',
             'set_as_default' => true,
         ]);
@@ -107,9 +107,9 @@ class UpdateCraterVersion400 extends Migration
 
     private function updateCompanySettings($user)
     {
-        $defaultInvoiceEmailBody = 'You have received a new invoice from <b>{COMPANY_NAME}</b>.</br>Please download using the button below:';
-        $defaultEstimateEmailBody = 'You have received a new estimate from <b>{COMPANY_NAME}</b>.</br>Please download using the button below:';
-        $defaultPaymentEmailBody = 'Thank you for the payment.</b></br>Please download your payment receipt using the button below:';
+        $defaultInvoiceEmailBody = 'You have received a new invoice from <b>{COMPANY_NAME}</b>.</br> Please download using the button below:';
+        $defaultEstimateEmailBody = 'You have received a new estimate from <b>{COMPANY_NAME}</b>.</br> Please download using the button below:';
+        $defaultPaymentEmailBody = 'Thank you for the payment.</b></br> Please download your payment receipt using the button below:';
         $billingAddressFormat = '<h3>{BILLING_ADDRESS_NAME}</h3><p>{BILLING_ADDRESS_STREET_1}</p><p>{BILLING_ADDRESS_STREET_2}</p><p>{BILLING_CITY}  {BILLING_STATE}</p><p>{BILLING_COUNTRY}  {BILLING_ZIP_CODE}</p><p>{BILLING_PHONE}</p>';
         $shippingAddressFormat = '<h3>{SHIPPING_ADDRESS_NAME}</h3><p>{SHIPPING_ADDRESS_STREET_1}</p><p>{SHIPPING_ADDRESS_STREET_2}</p><p>{SHIPPING_CITY}  {SHIPPING_STATE}</p><p>{SHIPPING_COUNTRY}  {SHIPPING_ZIP_CODE}</p><p>{SHIPPING_PHONE}</p>';
         $companyAddressFormat = '<h3><strong>{COMPANY_NAME}</strong></h3><p>{COMPANY_ADDRESS_STREET_1}</p><p>{COMPANY_ADDRESS_STREET_2}</p><p>{COMPANY_CITY} {COMPANY_STATE}</p><p>{COMPANY_COUNTRY}  {COMPANY_ZIP_CODE}</p><p>{COMPANY_PHONE}</p>';

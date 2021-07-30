c598c8c3df792860eddae3773544db10590702e4
Pascal Pischel <mail@pascal-pischel.de>
2021-07-30 21:17:15 +0200

    Adding "secure" Flag for all cookies as standard
    Adding "secure" Flag for all cookies as standard for security purposes.

--- config/session.php
+++ config/session.php
@@ -166,7 +166,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE'),
+    'secure' => env('SESSION_SECURE_COOKIE', true),
 
     /*
     |--------------------------------------------------------------------------

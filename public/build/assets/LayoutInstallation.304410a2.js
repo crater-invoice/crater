4438d54401905f61546231d8dae0b2af364b5dc1
Mohit Panjwani <mohit.panjvani@gmail.com>
2021-12-02 18:01:22 +0530

    new build 502

--- public/build/assets/LayoutInstallation.304410a2.js
+++
@@ -1,1 +0,0 @@
-import{N as t}from"./main.d5eaf1d9.js";import{r as s,o as r,c as a,b as e,t as c}from"./vendor.e9042f2c.js";const n={class:"h-screen h-screen-ios overflow-y-auto text-base"},i={class:"container mx-auto px-4"},l={setup(_){return(d,m)=>{const o=s("router-view");return r(),a("div",n,[e(t),c("div",i,[e(o)])])}}};export{l as default};

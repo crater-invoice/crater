8a7ea53ffbdbc56b886397e000bca5bd1dd57429
Mohit Panjwani <mohit.panjvani@gmail.com>
2021-12-03 21:13:44 +0530

    new build

--- public/build/assets/LayoutInstallation.8e7fb52b.js
+++
@@ -1,1 +0,0 @@
-import{N as t}from"./main.50c1f863.js";import{r as s,o as r,c,b as e,t as a}from"./vendor.e9042f2c.js";const n={class:"h-screen h-screen-ios overflow-y-auto text-base"},i={class:"container mx-auto px-4"},l={setup(_){return(m,p)=>{const o=s("router-view");return r(),c("div",n,[e(t),a("div",i,[e(o)])])}}};export{l as default};

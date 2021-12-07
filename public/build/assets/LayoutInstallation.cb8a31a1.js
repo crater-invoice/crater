e8b4339237d081fb403efb0224af0782d7981bfa
Mohit Panjwani <mohit.panjvani@gmail.com>
2021-12-07 17:28:49 +0530

    fix loading indicator and send invoice status error

---
+++ public/build/assets/LayoutInstallation.cb8a31a1.js
@@ -0,0 +1,1 @@
+import{N as t}from"./main.e180e539.js";import{r as s,o as r,c as a,b as e,t as c}from"./vendor.e9042f2c.js";const n={class:"h-screen h-screen-ios overflow-y-auto text-base"},i={class:"container mx-auto px-4"},u={setup(_){return(m,p)=>{const o=s("router-view");return r(),a("div",n,[e(t),c("div",i,[e(o)])])}}};export{u as default};

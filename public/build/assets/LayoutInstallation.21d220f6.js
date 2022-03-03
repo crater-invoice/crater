35da80103e2849dfedc8a3db4edc5f148a983a9a
Mohit Panjwani <mohit.panjvani@gmail.com>
2022-03-03 11:43:03 +0530

    new build 605

--- public/build/assets/LayoutInstallation.21d220f6.js
+++
@@ -1,1 +0,0 @@
-import{N as t}from"./NotificationRoot.b3b15121.js";import{r as s,o as a,e as r,f as o,h as c}from"./vendor.d12b5734.js";import"./main.a7fcb8c6.js";const n={class:"h-screen overflow-y-auto text-base"},i={class:"container mx-auto px-4"},u={setup(_){return(m,p)=>{const e=s("router-view");return a(),r("div",n,[o(t),c("div",i,[o(e)])])}}};export{u as default};

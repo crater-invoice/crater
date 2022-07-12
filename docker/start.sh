e74dcf48a3178aac7482c0d1194bd69d3508ca39
Venu Vardhan Reddy Tekula <venuvardhan.t@beehyv.com>
2022-07-12 16:15:02 +0530

    Use different approach for wait script (#5)
    This commit also fixes the image tags for both
    php and nginx services in the helm charts.

--- docker/start.sh
+++ docker/start.sh
@@ -6,13 +6,12 @@ envsubst < .env.template  > .env.example
 
 cp .env.example .env
 
-sh /etc/wait-for $DB_HOST:$DB_PORT
-
 php artisan key:generate --force
 php artisan storage:link || true
 
 if [ ${AUTO_INSTALL} == "true" ]
 then
+    wait-for-it --timeout=300 $DB_HOST:$DB_PORT
     php artisan migrate --force
     php artisan db:seed --force
     php artisan db:seed --class=DemoSeeder --force

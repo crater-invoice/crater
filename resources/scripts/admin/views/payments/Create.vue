fbbbde7fc96c2204715c980f21800bd5421c2d05
Max <contact@agencetwogether.fr>
2022-08-25 00:49:12 +0200

    Remove hardcoded Amount Due : xx below Invoice dropdown to locale translation

--- resources/scripts/admin/views/payments/Create.vue
+++ resources/scripts/admin/views/payments/Create.vue
@@ -99,7 +99,7 @@
             :label="$t('payments.invoice')"
             :help-text="
               selectedInvoice
-                ? `Due Amount: ${
+                ? `${t('customers.amount_due')} : ${
                     paymentStore.currentPayment.maxPayableAmount / 100
                   }`
                 : ''

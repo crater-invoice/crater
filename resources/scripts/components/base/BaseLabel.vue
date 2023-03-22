87495e8055ab27eed492e52587b6ee904ddfb3e6
varshapun <vdhinglani@gmail.com>
2023-03-22 10:16:06 +0530

    added dark-mode in  BaseInputGroup and BaseLabel

--- resources/scripts/components/base/BaseLabel.vue
+++ resources/scripts/components/base/BaseLabel.vue
@@ -1,5 +1,5 @@
 <template>
-  <label class="text-sm not-italic font-medium leading-5 text-primary-800">
+  <label class="text-sm not-italic font-medium leading-5 text-primary-800   dark:text-gray-300">
     <slot />
   </label>
 </template>

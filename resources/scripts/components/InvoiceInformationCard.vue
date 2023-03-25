235fb5494df63414dd60352b6f332d532fb58e6b
varshapun <vdhinglani@gmail.com>
2023-03-25 15:43:39 +0530

    add dark-mode in InvoiceInformationCard

--- resources/scripts/components/InvoiceInformationCard.vue
+++ resources/scripts/components/InvoiceInformationCard.vue
@@ -1,46 +1,46 @@
 <template>
-  <div class="bg-white shadow overflow-hidden rounded-lg mt-6">
+  <div class="bg-white shadow overflow-hidden rounded-lg mt-6 dark:bg-gray-800">
     <div class="px-4 py-5 sm:px-6">
-      <h3 class="text-lg leading-6 font-medium text-gray-900">
+      <h3 class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-400">
         {{ $t('invoices.invoice_information') }}
       </h3>
     </div>
     <div v-if="invoice" class="border-t border-gray-200 px-4 py-5 sm:p-0">
-      <dl class="sm:divide-y sm:divide-gray-200">
+      <dl class="sm:divide-y sm:divide-gray-200 dark:sm:divide-gray-500">
         <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-500">
+          <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">
             {{ $t('general.from') }}
           </dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2   dark:text-gray-100">
             {{ invoice.company.name }}
           </dd>
         </div>
         <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-500">
+          <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">
             {{ $t('general.to') }}
           </dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2   dark:text-gray-100">
             {{ invoice.customer.name }}
           </dd>
         </div>
         <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-500 capitalize">
+          <dt class="text-sm font-medium text-gray-500 capitalize dark:text-gray-400 ">
             {{ $t('invoices.paid_status').toLowerCase() }}
           </dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 ">
             <BaseInvoiceStatusBadge
               :status="invoice.paid_status"
-              class="px-3 py-1"
+              class="px-3 py-1 dark:text-gray-400"
             >
               {{ invoice.paid_status }}
             </BaseInvoiceStatusBadge>
           </dd>
         </div>
         <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-          <dt class="text-sm font-medium text-gray-500">
+          <dt class="text-sm font-medium text-gray-500 dark:text-gray-400">
             {{ $t('invoices.total') }}
           </dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 dark:border-gray-600 dark:bg-gray-700 dark:text-white">
             <BaseFormatMoney
               :currency="invoice.currency"
               :amount="invoice.total"
@@ -54,7 +54,7 @@
           <dt class="text-sm font-medium text-gray-500">
             {{ $t('invoices.notes') }}
           </dt>
-          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+          <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 dark:text-gray-100" >
             <span v-html="invoice.formatted_notes"></span>
           </dd>
         </div>

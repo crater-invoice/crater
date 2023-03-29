feda20431c7d5a99d892aebb9c500708e07866be
yashkanakiya <yashkanakiya281297@gmail.com>
2023-03-29 10:04:10 +0530

    add dark mode in DashboardStatsItem

--- resources/scripts/customer/views/dashboard/DashboardStatsItem.vue
+++ resources/scripts/customer/views/dashboard/DashboardStatsItem.vue
@@ -12,18 +12,24 @@
       hover:bg-gray-50
       xl:p-4
       lg:col-span-2
+      dark:backdrop-blur-xl
+      dark:shadow-glass
+      dark:border
+      dark:border-white/10
+      dark:bg-gray-800/70
     "
     :class="{ 'lg:!col-span-3': large }"
     :to="route"
   >
     <div>
-      <span class="text-xl font-semibold leading-tight text-black xl:text-3xl">
+      <span class="text-xl font-semibold leading-tight text-black xl:text-3xl dark:text-white">
         <slot />
       </span>
-      <span class="block mt-1 text-sm leading-tight text-gray-500 xl:text-lg">
+      <span class="block mt-1 text-sm leading-tight text-gray-500 xl:text-lg dark:text-gray-300">
         {{ label }}
       </span>
     </div>
+      <BaseDarkHighlight class="!bg-highlight/[.17] !top-5" />
     <div class="flex items-center">
       <component :is="iconComponent" class="w-10 h-10 xl:w-12 xl:h-12" />
     </div>

6740f6690300622d6300cb82e8e7dbc34ff27e92
yogesh-gohil <yogeshgohil1611@gmail.com>
2023-03-30 13:43:41 +0530

    fix remaining label for dark mode

--- resources/scripts/components/base/BaseInputGroup.vue
+++ resources/scripts/components/base/BaseInputGroup.vue
@@ -32,7 +32,7 @@
     </label>
     <div :class="inputContainerClasses">
       <slot></slot>
-      <span v-if="helpText" class="text-gray-500 text-xs mt-1 font-light">
+      <span v-if="helpText" class="text-gray-500 text-xs mt-1 font-light dark:text-gray-400">
         {{ helpText }}
       </span>
       <span v-if="error" class="block mt-0.5 text-sm text-red-500">

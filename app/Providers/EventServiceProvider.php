17351efacf79458d4b56594aaffd0e5792aa0960
Darko Gjorgjijoski <dg@darkog.com>
2024-01-28 00:16:28 +0100

    Remove old database updates

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,18 +2,10 @@
 
 namespace InvoiceShelf\Providers;
 
-use InvoiceShelf\Events\UpdateFinished;
-use InvoiceShelf\Listeners\Updates\v1\Version110;
-use InvoiceShelf\Listeners\Updates\v2\Version200;
-use InvoiceShelf\Listeners\Updates\v2\Version201;
-use InvoiceShelf\Listeners\Updates\v2\Version202;
-use InvoiceShelf\Listeners\Updates\v2\Version210;
-use InvoiceShelf\Listeners\Updates\v3\Version300;
-use InvoiceShelf\Listeners\Updates\v3\Version310;
-use InvoiceShelf\Listeners\Updates\v3\Version311;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use InvoiceShelf\Events\UpdateFinished;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -24,14 +16,7 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         UpdateFinished::class => [
-            Version110::class,
-            Version200::class,
-            Version201::class,
-            Version202::class,
-            Version210::class,
-            Version300::class,
-            Version310::class,
-            Version311::class,
+
         ],
         Registered::class => [
             SendEmailVerificationNotification::class,

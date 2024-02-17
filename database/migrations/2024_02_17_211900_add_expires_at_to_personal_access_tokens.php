5ba8639ac895f800c0b30e8fb1b425a9ac8f8168
Darius Kristapavicius <darius@darneta.lt>
2024-02-17 23:31:31 +0200

    Added expires_at column to personal_access_tokens table

---
+++ database/migrations/2024_02_17_211900_add_expires_at_to_personal_access_tokens.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->dateTime('expires_at')->nullable()->after('last_used_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->dropColumn('expires_at');
+        });
+    }
+};

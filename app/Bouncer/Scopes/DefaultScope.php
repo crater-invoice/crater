e73ab3d4eef0535aa9b1f21f47664bbc651d9f9c
gdarko <dg@darkog.com>
2024-02-18 00:13:24 +0100

    Override the default bouncer scope class with a temporary hotfix to the roles issue

--- app/Bouncer/Scopes/DefaultScope.php
+++ app/Bouncer/Scopes/DefaultScope.php
@@ -2,7 +2,37 @@
 
 namespace InvoiceShelf\Bouncer\Scopes;
 
-class DefaultScope
-{
+use Silber\Bouncer\Database\Scope\Scope;
 
+class DefaultScope extends Scope
+{
+    public function applyToModelQuery($query, $table = null)
+    {
+        if (is_null($this->scope) || $this->onlyScopeRelations) {
+            return $query;
+        }
+
+        if (is_null($table)) {
+            $table = $query->getModel()->getTable();
+        }
+
+        return $this->applyToQuery($query, $table);
+    }
+
+    public function applyToRelationQuery($query, $table)
+    {
+        if (is_null($this->scope)) {
+            return $query;
+        }
+
+        return $this->applyToQuery($query, $table);
+    }
+
+    protected function applyToQuery($query, $table)
+    {
+        return $query->where(function ($query) use ($table) {
+            $query->where("{$table}.scope", $this->scope)
+                ->orWhereNull("{$table}.scope");
+        });
+    }
 }
